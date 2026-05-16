3b5aeb59858f3bdfad3b981d2f14cd66a094dcb7
lightlivermorium <vladbndko.dev@gmail.com>
2026-05-16 20:25:36 +0300

    reformat the code

--- src/resources.js
+++ src/resources.js
@@ -87,22 +87,26 @@ const downloadResource = (
 
   const contentPromise
     = resource.url === pageUrl
-      ? (log('resources: reuse page html for %s', resource.url),
-      Promise.resolve(pageHtml))
-      : (log('resources: download %s', resource.url),
-      axios
-        .get(resource.url, { responseType: 'arraybuffer' })
-        .then((response) => {
-          log(
-            'resources: downloaded %s status=%d',
-            resource.url,
-            response.status,
-          )
-          return response.data
-        })
-        .catch((error) => {
-          throw normalizeRequestError('resource', resource.url, error)
-        }))
+      ? (
+        log('resources: reuse page html for %s', resource.url),
+        Promise.resolve(pageHtml)
+      )
+      : (
+        log('resources: download %s', resource.url),
+        axios
+          .get(resource.url, { responseType: 'arraybuffer' })
+          .then((response) => {
+            log(
+              'resources: downloaded %s status=%d',
+              resource.url,
+              response.status
+            )
+            return response.data
+          })
+          .catch((error) => {
+            throw normalizeRequestError('resource', resource.url, error)
+          })
+      )
 
   const targetPath = path.join(assetsDirPath, resource.filename)
 
