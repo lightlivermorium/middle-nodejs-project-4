852ffb440b17a4aa814ca9cb9b163093929a4fa7
lightlivermorium <vladbndko.dev@gmail.com>
2026-05-20 19:33:58 +0300

    fix tests

--- __tests__/filename.test.js
+++ __tests__/filename.test.js
@@ -1,4 +1,8 @@
 const makeFilename = require('../src/filename')
+const {
+  makeAssetFilename,
+  makeAssetsDirname,
+} = require('../src/filename')
 
 describe('makeFilename', () => {
   test('generates filename from url with path', () => {
@@ -19,5 +23,20 @@ describe('makeFilename', () => {
   test('ignores query string and hash', () => {
     const url = 'https://ru.hexlet.io/blog/about?name=test#section'
     expect(makeFilename(url)).toBe('ru-hexlet-io-blog-about.html')
+  })
+
+  test('generates assets directory name', () => {
+    const url = 'https://ru.hexlet.io/courses'
+    expect(makeAssetsDirname(url)).toBe('ru-hexlet-io-courses_files')
+  })
+
+  test('generates asset filename preserving extension', () => {
+    const url = 'https://ru.hexlet.io/assets/application.css'
+    expect(makeAssetFilename(url)).toBe('ru-hexlet-io-assets-application.css')
+  })
+
+  test('generates asset filename without extension as html', () => {
+    const url = 'https://ru.hexlet.io/courses'
+    expect(makeAssetFilename(url)).toBe('ru-hexlet-io-courses.html')
   })
 })
